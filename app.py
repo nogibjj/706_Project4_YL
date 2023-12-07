60da68e9a3c75e056d2de7c1b87d9dc073ca33f6
Yue Li <michaelli0914@outlook.com>
2023-12-07 01:41:29 +0000

    changed app design

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
+def home():
     return render_template("home.html", title="Home")
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title="About")
+    return render_template("about.html", title="About Us")
+
+
+@app.route("/api/openai")
+def prompt():
+    return render_template("prompt.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=23333)
